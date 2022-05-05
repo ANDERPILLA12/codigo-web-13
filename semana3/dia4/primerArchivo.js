0c17766b32c364ce2a26126319ffb648d063f35d
ANDERPILLA12 <apillacagregorio@gmail.com>
2022-05-05 17:51:56 -0500

    actualizacion05/05/2022

---
+++ semana3/dia4/primerArchivo.js
@@ -0,0 +1,16 @@
+
+const numero1 = prompt("ingrese numero1")
+const numero2 = prompt("ingrese numero2")
+
+// recuerden el ; no importa
+let resultado = 0;
+
+// 2. Concatenacion peros si son strings junto a numbers lo que hace es concatenar
+if (+numero1> +numero2){
+    console.log(" numero1 es mayor")
+}else{
+    console.log(" numero2 es mayor")
+}
+// otra propiedad del console.log poder darle un key a un valor
+
+
